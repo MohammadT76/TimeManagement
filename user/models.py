e27d7f754881bedea08544922a28964a6a5602db
Mohammad_Talaei <mohammadtalaei76@gmail.com>
2022-01-07 20:54:40 +0330

    add new features to admin panel .

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.contrib.auth import get_user_model  # ex) models.ForeignKey(get_user_model(), ...)
+from django.conf import settings
+
+
+# using a custom user model
+class User(AbstractUser):
+    pass
